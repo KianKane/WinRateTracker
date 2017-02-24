8f5b21c876d3ec89a7429f9c6a8378064c8913e3
Kian Kane <AmethystArchon@gmail.com>
2017-02-24 11:48:05 +0000

    Created a model.

---
+++ WinRateTracker/Model/IModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WinRateTracker.Model
+{
+    interface IModel
+    {
+        void InsertBuild(string name, string note, int archetypeID);
+        void UpdateBuild(int buildID, string name, string note, int archetypeID));
+        void DeleteBuild(int buildID);
+        void InsertArchetype(string name, string note);
+        void UpdateArchetype(int archetypeID, string name, string note);
+        void DeleteArchetype(int archetypeID);
+        void RecordMatch(int buildID, int archetypeID, bool victory);
+        int CountMatches(int? buildID, int? archetypeID, bool victory);
+    }
+}
---
+++ WinRateTracker/Model/Model.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace WinRateTracker.Model
+{
+    class Model : IModel
+    {
+        DatabaseDataSet dataSet;
+        DatabaseDataSetTableAdapters.BuildsTableAdapter buildsTableAdapter;
+        DatabaseDataSetTableAdapters.MatchesTableAdapter matchesTableAdapter;
+        DatabaseDataSetTableAdapters.ArchetypesTableAdapter archetypesTableAdapter;
+
+        public Model()
+        {
+            dataSet = new DatabaseDataSet();
+            dataSet.BeginInit();
+            dataSet.DataSetName = "DatabaseDataSet";
+            dataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            dataSet.EndInit();
+
+            buildsTableAdapter = new DatabaseDataSetTableAdapters.BuildsTableAdapter();
+            buildsTableAdapter.ClearBeforeFill = true;
+
+            matchesTableAdapter = new DatabaseDataSetTableAdapters.MatchesTableAdapter();
+            matchesTableAdapter.ClearBeforeFill = true;
+
+            archetypesTableAdapter = new DatabaseDataSetTableAdapters.ArchetypesTableAdapter();
+            archetypesTableAdapter.ClearBeforeFill = true;
+        }
+
+        public void InsertBuild(string name, string note, int archetypeID)
+        {
+            buildsTableAdapter.InsertQuery(name, note, archetypeID);
+            FillDataSet();
+        }
+
+        public void UpdateBuild(int buildID, string name, string note, int archetypeID)
+        {
+            buildsTableAdapter.UpdateQuery(name, note, archetypeID, buildID);
+            FillDataSet();
+        }
+
+        public void DeleteBuild(int buildID)
+        {
+            buildsTableAdapter.DeleteQuery(buildID);
+            FillDataSet();
+        }
+
+        public void InsertArchetype(string name, string note)
+        {
+            archetypesTableAdapter.InsertQuery(name, note);
+            FillDataSet();
+        }
+
+        public void UpdateArchetype(int archetypeID, string name, string note)
+        {
+            archetypesTableAdapter.UpdateQuery(name, note, archetypeID);
+            FillDataSet();
+        }
+
+        public void DeleteArchetype(int archetypeID)
+        {
+            archetypesTableAdapter.DeleteQuery(archetypeID);
+            FillDataSet();
+        }
+
+        public void RecordMatch(int buildID, int archetypeID, bool victory)
+        {
+            matchesTableAdapter.RecordMatchQuery(buildID, archetypeID, victory);
+            FillDataSet();
+        }
+
+        public int CountMatches(int? buildID, int? archetypeID, bool victory)
+        {
+            int matches = 0;
+            if (buildID != null && archetypeID != null)
+                matches = (int)matchesTableAdapter.CountMatchesQuery((int)buildID, (int)archetypeID, victory);
+            else if (archetypeID != null)
+                matchesTableAdapter.CountMatchesAllBuildsQuery((int)archetypeID, victory);
+            else if (buildID != null)
+                matchesTableAdapter.CountMatchesAllArchetypesQuery((int)buildID, victory);
+            else
+                matchesTableAdapter.CountMatchesAllQuery(victory);
+            return matches;
+        }
+
+        private void FillDataSet()
+        {
+            buildsTableAdapter.Fill(dataSet.Builds);
+            matchesTableAdapter.Fill(dataSet.Matches);
+            archetypesTableAdapter.Fill(dataSet.Archetypes);
+        }
+    }
+}
