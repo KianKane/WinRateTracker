5e01c75b63fab96f93eaba3dd14c0d7dfc057a79
Kian Kane <AmethystArchon@gmail.com>
2017-02-24 11:53:23 +0000

    Fixed issue with IModel interface.

--- WinRateTracker/Model/IModel.cs
+++ WinRateTracker/Model/IModel.cs
@@ -3,7 +3,7 @@
     interface IModel
     {
         void InsertBuild(string name, string note, int archetypeID);
-        void UpdateBuild(int buildID, string name, string note, int archetypeID));
+        void UpdateBuild(int buildID, string name, string note, int archetypeID);
         void DeleteBuild(int buildID);
         void InsertArchetype(string name, string note);
         void UpdateArchetype(int archetypeID, string name, string note);
