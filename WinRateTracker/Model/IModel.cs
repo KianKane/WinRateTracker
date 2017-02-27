6451920fb4bd7c130d9bf089ae3fde46cf1332cd
Kian Kane <AmethystArchon@gmail.com>
2017-02-27 15:36:43 +0000

    Added internal documentation and cleaned up code.

--- WinRateTracker/Model/IModel.cs
+++ WinRateTracker/Model/IModel.cs
@@ -1,21 +1,86 @@
 ﻿namespace WinRateTracker.Model
 {
+    /// <summary>
+    /// This interface defines the outward facing interaction contract of the project model.
+    /// </summary>
     public interface IModel
     {
+        /// <summary> Get the DatabaseDataSet instance used by this model.  Views use this for data binding, presenters should not use this method. </summary>
+        /// <returns> The DatabaseDataSet instance used by this model. </returns>
         DatabaseDataSet GetDataSet();
+
+        /// <summary> Adds a new build and inserts it into the database. </summary>
+        /// <param name="name"> The name of the new build. </param>
+        /// <param name="note"> The note attatched to the new build. </param>
+        /// <param name="archetypeID"> The archetypeID of the archetype attatched to the new build. </param>
         void InsertBuild(string name, string note, int archetypeID);
+
+        /// <summary> Modifies a specified build.  This cannot be used to change the archetype attatched to the build. </summary>
+        /// <param name="buildID"> The ID of the build to be modified. </param>
+        /// <param name="name"> The new name to be assigned to the build. </param>
+        /// <param name="note"> The new note to be attatched to the build. </param>
         void UpdateBuild(int buildID, string name, string note);
+
+        /// <summary> Deletes a specified build, removing it from the database. </summary>
+        /// <param name="buildID"> The ID of the build to be deleted. </param>
         void DeleteBuild(int buildID);
+
+        /// <summary> Adds a new archetype and inserts it into the database. </summary>
+        /// <param name="name"> The name of the new archetype. </param>
+        /// <param name="note"> The note attatched to the new archetype. </param>
         void InsertArchetype(string name, string note);
+
+        /// <summary> Modifies a specified archetype. </summary>
+        /// <param name="archetypeID"> The ID of the archetype to be modified. </param>
+        /// <param name="name"> The new name to be assigned to the archetype. </param>
+        /// <param name="note"> The new note to be attatched to the archetype. </param>
         void UpdateArchetype(int archetypeID, string name, string note);
+
+        /// <summary> Deletes a specified archetype, removing it from the database. </summary>
+        /// <param name="archetypeID"> The ID of the archetype to be deleted. </param>
         void DeleteArchetype(int archetypeID);
+
+        /// <summary> Records a match and inserts it into the database. </summary>
+        /// <param name="buildID"> The ID of the build used in the match. </param>
+        /// <param name="archetypeID"> The ID of the archetype played against. </param>
+        /// <param name="victory"> The result of the game. (TRUE = Victory, FALSE = Defeat) </param>
         void RecordMatch(int buildID, int archetypeID, bool victory);
+
+        /// <summary> Counts and returns the number of matches played meeting the requirements indicated by the parameters. </summary>
+        /// <param name="buildID"> The ID of the build used in the matches to be counted. (NULL = All Builds) </param>
+        /// <param name="archetypeID"> The ID of the archetype used in the matches to be counted. (NULL = All Archetypes) </param>
+        /// <param name="victory"> The result of the games to be counted. (TRUE = Victory, FALSE = Defeat) </param>
+        /// <returns> The number of matches played meeting the requirements indicated by the parameters. </returns>
         int CountMatches(int? buildID, int? archetypeID, bool victory);
+
+        /// <summary> Gets and returns the name of the archetype with the passed ID. </summary>
+        /// <param name="archetypeID"> The ID of the archetype to find. </param>
+        /// <returns> The name of the archetype with the passed ID. </returns>
         string GetArchetypeName(int archetypeID);
+
+        /// <summary> Gets and returns the note attatched to the archetype with the passed ID. </summary>
+        /// <param name="archetypeID"> The ID of the archetype to find. </param>
+        /// <returns> The note attatched to the archetype with the passed ID. </returns>
         string GetArchetypeNote(int archetypeID);
+
+        /// <summary> Gets and returns the name of the build with the passed ID. </summary>
+        /// <param name="buildID"> The ID of the build to find. </param>
+        /// <returns> The name of the build with the passed ID. </returns>
         string GetBuildName(int buildID);
+
+        /// <summary> Gets and returns the note attatched to the build with the passed ID. </summary>
+        /// <param name="buildID"> The ID of the build to find. </param>
+        /// <returns> The note attatched to the build with the passed ID. </returns>
         string GetBuildNote(int buildID);
+
+        /// <summary> Gets and returns the archetypeID of the archetype connected to the build with the passed ID. </summary>
+        /// <param name="buildID"> The ID of the build to find. </param>
+        /// <returns> The archetypeID of the archetype connected to the build with the passed ID. </returns>
         int GetBuildArchetypeID(int buildID);
+
+        /// <summary> Gets and returns the total number of archetypes. 
+        /// NOTE: Could replace with NoArchetypes with a boolean return type as this method is currently only used to check if there are no archetypes </summary>
+        /// <returns> The total number of archetypes. </returns>
         int GetArchetypeCount();
     }
 }
--- WinRateTracker/Model/Model.cs
+++ WinRateTracker/Model/Model.cs
@@ -4,9 +4,44 @@ using System.Windows.Forms;
 
 namespace WinRateTracker.Model
 {
+    /// <summary>
+    /// This class is a concrete singleton implementation of the IModel interface.
+    /// This class allows other classes to access the database through it without exposing the details of the database connection.
+    /// </summary>
     class Model : IModel
     {
-        private static Model _instance;
+        private static Model _instance; // Private singleton instance variable, this should not be used anywhere except inside the Instance property getter.
+        private DatabaseDataSet dataSet;
+        private DatabaseDataSetTableAdapters.BuildsTableAdapter buildsTableAdapter;
+        private DatabaseDataSetTableAdapters.MatchesTableAdapter matchesTableAdapter;
+        private DatabaseDataSetTableAdapters.ArchetypesTableAdapter archetypesTableAdapter;
+
+        /// <summary>
+        /// Constructor.
+        /// Sets up dataset and table adapters.
+        /// </summary>
+        private Model()
+        {
+            // Set up data set
+            dataSet = new DatabaseDataSet();
+            dataSet.BeginInit();
+            dataSet.DataSetName = "DatabaseDataSet";
+            dataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            dataSet.EndInit();
+            // Set up table adapters
+            buildsTableAdapter = new DatabaseDataSetTableAdapters.BuildsTableAdapter();
+            buildsTableAdapter.ClearBeforeFill = true;
+            matchesTableAdapter = new DatabaseDataSetTableAdapters.MatchesTableAdapter();
+            matchesTableAdapter.ClearBeforeFill = true;
+            archetypesTableAdapter = new DatabaseDataSetTableAdapters.ArchetypesTableAdapter();
+            archetypesTableAdapter.ClearBeforeFill = true;
+            // Fill table adapters
+            buildsTableAdapter.Fill(dataSet.Builds);
+            matchesTableAdapter.Fill(dataSet.Matches);
+            archetypesTableAdapter.Fill(dataSet.Archetypes);
+        }
+
+        /// <summary> Singleton instance property. </summary>
         public static Model Instance
         {
             get
@@ -17,50 +52,27 @@ namespace WinRateTracker.Model
             }
         }
 
-        DatabaseDataSet dataSet;
-        DatabaseDataSetTableAdapters.BuildsTableAdapter buildsTableAdapter;
-        DatabaseDataSetTableAdapters.MatchesTableAdapter matchesTableAdapter;
-        DatabaseDataSetTableAdapters.ArchetypesTableAdapter archetypesTableAdapter;
-
-        private Model()
-        {
-            dataSet = new DatabaseDataSet();
-            dataSet.BeginInit();
-            dataSet.DataSetName = "DatabaseDataSet";
-            dataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            dataSet.EndInit();
-
-            buildsTableAdapter = new DatabaseDataSetTableAdapters.BuildsTableAdapter();
-            buildsTableAdapter.ClearBeforeFill = true;
-
-            matchesTableAdapter = new DatabaseDataSetTableAdapters.MatchesTableAdapter();
-            matchesTableAdapter.ClearBeforeFill = true;
-
-            archetypesTableAdapter = new DatabaseDataSetTableAdapters.ArchetypesTableAdapter();
-            archetypesTableAdapter.ClearBeforeFill = true;
-
-            buildsTableAdapter.Fill(dataSet.Builds);
-            matchesTableAdapter.Fill(dataSet.Matches);
-            archetypesTableAdapter.Fill(dataSet.Archetypes);
-        }
-
+        /// <summary> Interface realization method.  See interface for documentation. </summary>
         public DatabaseDataSet GetDataSet()
         {
             return dataSet;
         }
 
+        /// <summary> Interface realization method.  See interface for documentation. </summary>
         public void InsertBuild(string name, string note, int archetypeID)
         {
             buildsTableAdapter.InsertQuery(name, note, archetypeID);
             buildsTableAdapter.Fill(dataSet.Builds);
         }
 
+        /// <summary> Interface realization method.  See interface for documentation. </summary>
         public void UpdateBuild(int buildID, string name, string note)
         {
             buildsTableAdapter.UpdateQuery(name, note, buildID);
             buildsTableAdapter.Fill(dataSet.Builds);
         }
 
+        /// <summary> Interface realization method.  See interface for documentation. </summary>
         public void DeleteBuild(int buildID)
         {
             buildsTableAdapter.DeleteQuery(buildID);
@@ -68,18 +80,21 @@ namespace WinRateTracker.Model
             matchesTableAdapter.Fill(dataSet.Matches);
         }
 
+        /// <summary> Interface realization method.  See interface for documentation. </summary>
         public void InsertArchetype(string name, string note)
         {
             archetypesTableAdapter.InsertQuery(name, note);
             archetypesTableAdapter.Fill(dataSet.Archetypes);
         }
 
+        /// <summary> Interface realization method.  See interface for documentation. </summary>
         public void UpdateArchetype(int archetypeID, string name, string note)
         {
             archetypesTableAdapter.UpdateQuery(name, note, archetypeID);
             archetypesTableAdapter.Fill(dataSet.Archetypes);
         }
 
+        /// <summary> Interface realization method.  See interface for documentation. </summary>
         public void DeleteArchetype(int archetypeID)
         {
             archetypesTableAdapter.DeleteQuery(archetypeID);
@@ -88,26 +103,29 @@ namespace WinRateTracker.Model
             archetypesTableAdapter.Fill(dataSet.Archetypes);
         }
 
+        /// <summary> Interface realization method.  See interface for documentation. </summary>
         public void RecordMatch(int buildID, int archetypeID, bool victory)
         {
             matchesTableAdapter.RecordMatchQuery(buildID, archetypeID, victory);
             matchesTableAdapter.Fill(dataSet.Matches);
         }
 
+        /// <summary> Interface realization method.  See interface for documentation. </summary>
         public int CountMatches(int? buildID, int? archetypeID, bool victory)
         {
             int matches = 0;
             if (buildID == null && archetypeID == null)
-                matches = (int)matchesTableAdapter.CountMatchesAllQuery(victory);
+                matches = (int)matchesTableAdapter.CountMatchesAllQuery(victory); // All builds and all archetypes
             else if (buildID == null)
-                matches = (int)matchesTableAdapter.CountMatchesAllBuildsQuery((int)archetypeID, victory);
+                matches = (int)matchesTableAdapter.CountMatchesAllBuildsQuery((int)archetypeID, victory); // All builds but specific archetype
             else if (archetypeID == null)
-                matches = (int)matchesTableAdapter.CountMatchesAllArchetypesQuery((int)buildID, victory);
+                matches = (int)matchesTableAdapter.CountMatchesAllArchetypesQuery((int)buildID, victory); // All archetypes but specific build
             else
-                matches = (int)matchesTableAdapter.CountMatchesQuery((int)buildID, (int)archetypeID, victory);
+                matches = (int)matchesTableAdapter.CountMatchesQuery((int)buildID, (int)archetypeID, victory); // Specific build and archetype
             return matches;
         }
 
+        /// <summary> Interface realization method.  See interface for documentation. </summary>
         public string GetArchetypeName(int archetypeID)
         {
             DataRow[] rows = dataSet.Archetypes.Select("archetypeID = " + archetypeID);
@@ -117,6 +135,7 @@ namespace WinRateTracker.Model
             return row["name"].ToString();
         }
 
+        /// <summary> Interface realization method.  See interface for documentation. </summary>
         public string GetArchetypeNote(int archetypeID)
         {
             DataRow[] rows = dataSet.Archetypes.Select("archetypeID = " + archetypeID);
@@ -126,6 +145,7 @@ namespace WinRateTracker.Model
             return row["note"].ToString() ?? "";
         }
 
+        /// <summary> Interface realization method.  See interface for documentation. </summary>
         public string GetBuildName(int buildID)
         {
             DataRow[] rows = dataSet.Builds.Select("buildID = " + buildID);
@@ -135,6 +155,7 @@ namespace WinRateTracker.Model
             return row["name"].ToString();
         }
 
+        /// <summary> Interface realization method.  See interface for documentation. </summary>
         public string GetBuildNote(int buildID)
         {
             DataRow[] rows = dataSet.Builds.Select("buildID = " + buildID);
@@ -144,6 +165,7 @@ namespace WinRateTracker.Model
             return row["note"].ToString() ?? "";
         }
 
+        /// <summary> Interface realization method.  See interface for documentation. </summary>
         public int GetBuildArchetypeID(int buildID)
         {
             DataRow[] rows = dataSet.Builds.Select("buildID = " + buildID);
@@ -153,6 +175,7 @@ namespace WinRateTracker.Model
             return (int)row["archetypeID"];
         }
 
+        /// <summary> Interface realization method.  See interface for documentation. </summary>
         public int GetArchetypeCount()
         {
             return dataSet.Archetypes.Count;
