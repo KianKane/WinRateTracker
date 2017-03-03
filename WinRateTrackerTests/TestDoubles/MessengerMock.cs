b27c55311a96d04f5f2972ac1fa92afd558a60e2
Kian Kane <AmethystArchon@gmail.com>
2017-03-03 10:47:33 +0000

    Changed the default return of Prompt and added internal documentation.

--- WinRateTrackerTests/TestDoubles/MessengerMock.cs
+++ WinRateTrackerTests/TestDoubles/MessengerMock.cs
@@ -2,9 +2,16 @@
 
 namespace WinRateTrackerTests.TestDoubles
 {
+    /// <summary>
+    /// An empty stub implementation of IMessenger to be used for test purposes.
+    /// </summary>
     class MessengerStub : IMessenger
     {
+        /// <summary> Empty stub implementation. </summary>
         public void Message(string title, string message){}
-        public bool Prompt(string title, string message){ return false; }
+
+        /// <summary> Empty stub implementation.  Always returns true. </summary>
+        /// <returns> Always returns true. </returns>
+        public bool Prompt(string title, string message){ return true; }
     }
 }
