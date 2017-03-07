ba204494f912bc5473fcb22784f5244bad508631
Kian Kane <AmethystArchon@gmail.com>
2017-03-07 11:54:09 +0000

    Added some unit tests for the home presenter class.

---
+++ WinRateTrackerTests/TestDoubles/HomeMock.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using WinRateTracker.View;
+
+namespace WinRateTrackerTests.TestDoubles
+{
+    /// <summary>
+    /// This class acts as a mock for the Home form.
+    /// </summary>
+    class HomeMock : IHomeView
+    {
+        // Events
+        public event Action RecordVictory;
+        public event Action RecordDefeat;
+        public event Action UpdateStatistics;
+        public event Action NewBuild;
+        public event Action UpdateBuild;
+        public event Action DeleteBuild;
+        public event Action NewArchetype;
+        public event Action UpdateArchetype;
+        public event Action DeleteArchetype;
+
+        private string winRate;
+        private string wins;
+        private string losses;
+        private int? selectedBuildID;
+        private int? selectedArchetypeID;
+        private bool allBuilds;
+        private bool allArchetypes;
+        private bool setupDialogShown;
+        private bool newBuildDialogShown;
+        private bool updateBuildDialogShown;
+        private bool newArchetypeDialogShown; 
+        private bool updateArchetypeDialogShown;
+
+        /// <summary> Constructor. </summary>
+        public HomeMock()
+        {
+            // Set up initial values
+            winRate = string.Empty;
+            wins = string.Empty;
+            losses = string.Empty;
+            selectedBuildID = null;
+            selectedArchetypeID = null;
+            allBuilds = false;
+            allArchetypes = false;
+            setupDialogShown = false;
+            newBuildDialogShown = false;
+            updateBuildDialogShown = false;
+            newArchetypeDialogShown = false;
+            updateArchetypeDialogShown = false;
+    }
+
+        /// <summary> Used by testing classes to check if the setup dialog would have been shown by the view. </summary>
+        public bool SetupDialogShown
+        {
+            get { return setupDialogShown; }
+        }
+
+        /// <summary> Used by testing classes to check if the new build dialog would have been shown by the view. </summary>
+        public bool NewBuildDialogShown
+        {
+            get { return newBuildDialogShown; }
+        }
+
+        /// <summary> Used by testing classes to check if the update build dialog would have been shown by the view. </summary>
+        public bool UpdateBuildDialogShown
+        {
+            get { return updateBuildDialogShown; }
+        }
+
+        /// <summary> Used by testing classes to check if the new archetype dialog would have been shown by the view. </summary>
+        public bool NewArchetypeDialogShown
+        {
+            get { return newArchetypeDialogShown; }
+        }
+
+        /// <summary> Used by testing classes to check if the update archetype dialog would have been shown by the view. </summary>
+        public bool UpdateArchetypeDialogShown
+        {
+            get { return updateArchetypeDialogShown; }
+        }
+
+        /// <summary> Interface realization property.  See interface for documentation. </summary>
+        public string WinRate
+        {
+            set { winRate = value; }
+            get { return winRate; } // Getter used for testing purposes, it is not required by the interface and only exists in this mock.
+        }
+
+        /// <summary> Interface realization property.  See interface for documentation. </summary>
+        public string Wins
+        {
+            set { wins = value; }
+            get { return wins; } // Getter used for testing purposes, it is not required by the interface and only exists in this mock.
+        }
+
+        /// <summary> Interface realization property.  See interface for documentation. </summary>
+        public string Losses
+        {
+            set { losses = value; }
+            get { return losses; } // Getter used for testing purposes, it is not required by the interface and only exists in this mock.
+        }
+
+        /// <summary> Interface realization property.  See interface for documentation. </summary>
+        public int? SelectedBuildID
+        {
+            get { return selectedBuildID; }
+            set { selectedBuildID = value; } // Setter used for testing purposes, it is not required by the interface and only exists in this mock.
+        }
+
+        /// <summary> Interface realization property.  See interface for documentation. </summary>
+        public int? SelectedArchetypeID
+        {
+            get { return selectedArchetypeID; }
+            set { selectedArchetypeID = value; } // Setter used for testing purposes, it is not required by the interface and only exists in this mock.
+        }
+
+        /// <summary> Interface realization property.  See interface for documentation. </summary>
+        public bool AllBuilds
+        {
+            get { return allBuilds; }
+            set { allBuilds = value; } // Setter used for testing purposes, it is not required by the interface and only exists in this mock.
+        }
+
+        /// <summary> Interface realization property.  See interface for documentation. </summary>
+        public bool AllArchetypes
+        {
+            get { return allArchetypes; }
+            set { allArchetypes = value; } // Setter used for testing purposes, it is not required by the interface and only exists in this mock.
+        }
+
+        /// <summary> Interface realization property.  See interface for documentation. </summary>
+        public void ShowSetupDialog()
+        {
+            setupDialogShown = true;
+        }
+
+        /// <summary> Interface realization property.  See interface for documentation. </summary>
+        public void ShowNewBuildDialog()
+        {
+            newBuildDialogShown = true;
+        }
+
+        /// <summary> Interface realization property.  See interface for documentation. </summary>
+        public void ShowUpdateBuildDialog()
+        {
+            updateBuildDialogShown = true;
+        }
+
+        /// <summary> Interface realization property.  See interface for documentation. </summary>
+        public void ShowNewArchetypeDialog()
+        {
+            newArchetypeDialogShown = true;
+        }
+
+        /// <summary> Interface realization property.  See interface for documentation. </summary>
+        public void ShowUpdateArchetypeDialog()
+        {
+            updateArchetypeDialogShown = true;
+        }
+
+        /// <summary> Used by testing classes to invoke the RecordVictory event. </summary>
+        public void RecordVictory_Invoke()
+        {
+            RecordVictory?.Invoke();
+        }
+
+        /// <summary> Used by testing classes to invoke the RecordDefeat event. </summary>
+        public void RecordDefeat_Invoke()
+        {
+            RecordDefeat?.Invoke();
+        }
+
+        /// <summary> Used by testing classes to invoke the UpdateStatistics event. </summary>
+        public void UpdateStatistics_Invoke()
+        {
+            UpdateStatistics?.Invoke();
+        }
+
+        /// <summary> Used by testing classes to invoke the NewBuild event. </summary>
+        public void NewBuild_Invoke()
+        {
+            NewBuild?.Invoke();
+        }
+
+        /// <summary> Used by testing classes to invoke the UpdateBuild event. </summary>
+        public void UpdateBuild_Invoke()
+        {
+            UpdateBuild?.Invoke();
+        }
+
+        /// <summary> Used by testing classes to invoke the DeleteBuild event. </summary>
+        public void DeleteBuild_Invoke()
+        {
+            DeleteBuild?.Invoke();
+        }
+
+        /// <summary> Used by testing classes to invoke the NewArchetype event. </summary>
+        public void NewArchetype_Invoke()
+        {
+            NewArchetype?.Invoke();
+        }
+
+        /// <summary> Used by testing classes to invoke the UpdateArchetype event. </summary>
+        public void UpdateArchetype_Invoke()
+        {
+            UpdateArchetype?.Invoke();
+        }
+
+        /// <summary> Used by testing classes to invoke the DeleteArchetype event. </summary>
+        public void DeleteArchetype_Invoke()
+        {
+            DeleteArchetype?.Invoke();
+        }
+    }
+}
---
+++ WinRateTrackerTests/UnitTests/HomePresenterTests.cs
@@ -0,0 +1,200 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinRateTracker.Presenter;
+using WinRateTrackerTests.TestDoubles;
+
+namespace WinRateTrackerTests.UnitTests
+{
+    /// <summary>
+    /// This class is responsible for unit testing the home presenter.
+    /// </summary>
+    [TestClass]
+    public class HomePresenterTests
+    {
+        // Test doubles
+        MessengerMock messenger;
+        ModelMock model;
+
+        /// <summary> Runs before each test. </summary>
+        [TestInitialize]
+        public void Initialize()
+        {
+            messenger = new MessengerMock();
+            model = new ModelMock();
+        }
+
+        /// <summary> Runs after each test. </summary>
+        [TestCleanup]
+        public void Cleanup()
+        {
+            messenger = null;
+            model = null;
+        }
+
+        #region HomePresenter_Constructor
+        /// <summary>
+        /// Tests the constructor.
+        /// Ensures that the presenter is properly created.
+        /// Ensures that the setup dialog is shown when no archetypes exist within the model.
+        /// </summary>
+        [TestMethod]
+        public void HomePresenter_Constructor_NoExistingArchetypes()
+        {
+            HomeMock view = new HomeMock();
+            HomePresenter presenter = new HomePresenter(view, messenger, model);
+            Assert.IsNotNull(presenter);
+            Assert.IsTrue(view.SetupDialogShown);
+        }
+
+        /// <summary>
+        /// Tests the constructor.
+        /// Ensures that the presenter is properly created
+        /// Ensures that the setup dialog is not shown when archetypes do exist within the model.
+        /// </summary>
+        [TestMethod]
+        public void HomePresenter_Constructor_ExistingArchetypes()
+        {
+            model.InsertArchetype("Sample Archetype 1", "Sample Note");
+            HomeMock view = new HomeMock();
+            HomePresenter presenter = new HomePresenter(view, messenger, model);
+            Assert.IsNotNull(presenter);
+            Assert.IsFalse(view.SetupDialogShown);
+        }
+        #endregion
+
+        #region HomePresenter_RecordVictory
+        /// <summary>
+        /// Tests the record victory event method.
+        /// Ensures that the user is prompted for confirmation.
+        /// Ensures that a match is added.
+        /// </summary>
+        [TestMethod]
+        public void HomePresenter_RecordVictory_Typical()
+        {
+            HomeMock view = new HomeMock();
+            HomePresenter presenter = new HomePresenter(view, messenger, model);
+            int countBefore = model.matches.Count;
+            view.SelectedBuildID = 1;
+            view.SelectedArchetypeID = 1;
+            view.RecordVictory_Invoke();
+            Assert.AreEqual(new MessengerMock.MessageRecord("Confirmation", "Are you sure you want to record this victory?", true), messenger.Messages.Peek());
+            Assert.AreEqual(countBefore + 1, model.matches.Count);
+        }
+
+        /// <summary>
+        /// Tests the record victory event method.
+        /// Ensures that the user is told that the action was a falure.
+        /// Ensures that no new match is added.
+        /// </summary>
+        [TestMethod]
+        public void HomePresenter_RecordVictory_NoBuild()
+        {
+            HomeMock view = new HomeMock();
+            HomePresenter presenter = new HomePresenter(view, messenger, model);
+            int countBefore = model.matches.Count;
+            view.SelectedBuildID = null;
+            view.SelectedArchetypeID = 1;
+            view.RecordVictory_Invoke();
+            Assert.AreEqual(new MessengerMock.MessageRecord("Unable to record match", "No build is currently selected.", false), messenger.Messages.Peek());
+            Assert.AreEqual(countBefore, model.matches.Count);
+        }
+
+        /// <summary>
+        /// Tests the record victory event method.
+        /// Ensures that the user is told that the action was a falure.
+        /// Ensures that no new match is added.
+        /// </summary>
+        [TestMethod]
+        public void HomePresenter_RecordVictory_NoArchetype()
+        {
+            HomeMock view = new HomeMock();
+            HomePresenter presenter = new HomePresenter(view, messenger, model);
+            int countBefore = model.matches.Count;
+            view.SelectedBuildID = 1;
+            view.SelectedArchetypeID = null;
+            view.RecordVictory_Invoke();
+            Assert.AreEqual(new MessengerMock.MessageRecord("Unable to record match", "No archetype is currently selected.", false), messenger.Messages.Peek());
+            Assert.AreEqual(countBefore, model.matches.Count);
+        }
+        #endregion
+
+        /// <summary>
+        /// Tests the record defeat event method.
+        /// </summary>
+        [TestMethod]
+        public void HomePresenter_RecordDefeat()
+        {
+            HomeMock view = new HomeMock();
+            HomePresenter presenter = new HomePresenter(view, messenger, model);
+        }
+
+        /// <summary>
+        /// Tests the update statistics event method.
+        /// </summary>
+        [TestMethod]
+        public void HomePresenter_UpdateStatistics()
+        {
+            HomeMock view = new HomeMock();
+            HomePresenter presenter = new HomePresenter(view, messenger, model);
+        }
+
+        /// <summary>
+        /// Tests the new build event method.
+        /// </summary>
+        [TestMethod]
+        public void HomePresenter_NewBuild()
+        {
+            HomeMock view = new HomeMock();
+            HomePresenter presenter = new HomePresenter(view, messenger, model);
+        }
+
+        /// <summary>
+        /// Tests the update build event method.
+        /// </summary>
+        [TestMethod]
+        public void HomePresenter_UpdateBuild()
+        {
+            HomeMock view = new HomeMock();
+            HomePresenter presenter = new HomePresenter(view, messenger, model);
+        }
+
+        /// <summary>
+        /// Tests the delete build event method.
+        /// </summary>
+        [TestMethod]
+        public void HomePresenter_DeleteBuild()
+        {
+            HomeMock view = new HomeMock();
+            HomePresenter presenter = new HomePresenter(view, messenger, model);
+        }
+
+        /// <summary>
+        /// Tests the new archetype event method.
+        /// </summary>
+        [TestMethod]
+        public void HomePresenter_NewArchetype()
+        {
+            HomeMock view = new HomeMock();
+            HomePresenter presenter = new HomePresenter(view, messenger, model);
+        }
+
+        /// <summary>
+        /// Tests the update archetype event method.
+        /// </summary>
+        [TestMethod]
+        public void HomePresenter_UpdateArchetype()
+        {
+            HomeMock view = new HomeMock();
+            HomePresenter presenter = new HomePresenter(view, messenger, model);
+        }
+
+        /// <summary>
+        /// Tests the delete archetype event method.
+        /// </summary>
+        [TestMethod]
+        public void HomePresenter_DeleteArchetype()
+        {
+            HomeMock view = new HomeMock();
+            HomePresenter presenter = new HomePresenter(view, messenger, model);
+        }
+    }
+}
