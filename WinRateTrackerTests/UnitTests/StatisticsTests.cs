e8b03d56da537ca003e763538e35974d0348914e
Kian Kane <AmethystArchon@gmail.com>
2017-04-18 15:06:24 +0100

    Renamed functions in StatisticsTests to fit naming scheme and moved StatisticsTests into the UnitTests folder.

--- WinRateTrackerTests/UnitTests/StatisticsTests.cs
+++ WinRateTrackerTests/UnitTests/StatisticsTests.cs
@@ -8,7 +8,7 @@ namespace WinRateTrackerTests
     public class StatisticsTests
     {
         [TestMethod]
-        public void CalculateWinRate_Typical()
+        public void Statistics_CalculateWinRate_Typical()
         {
             int wins = 25;
             int losses = 50;
@@ -18,7 +18,7 @@ namespace WinRateTrackerTests
         }
 
         [TestMethod]
-        public void CalculateWinRate_NonTerminating()
+        public void Statistics_CalculateWinRate_NonTerminating()
         {
             int wins = 1;
             int losses = 3;
@@ -28,7 +28,7 @@ namespace WinRateTrackerTests
         }
 
         [TestMethod]
-        public void CalculateWinRate_LargeNumbers()
+        public void Statistics_CalculateWinRate_LargeNumbers()
         {
             int wins = 513215;
             int losses = 541242;
@@ -38,7 +38,7 @@ namespace WinRateTrackerTests
         }
 
         [TestMethod]
-        public void CalculateWinRate_ZeroWins()
+        public void Statistics_CalculateWinRate_ZeroWins()
         {
             int wins = 0;
             int losses = 35;
@@ -48,7 +48,7 @@ namespace WinRateTrackerTests
         }
 
         [TestMethod]
-        public void CalculateWinRate_ZeroLosses()
+        public void Statistics_CalculateWinRate_ZeroLosses()
         {
             int wins = 35;
             int losses = 0;
@@ -58,7 +58,7 @@ namespace WinRateTrackerTests
         }
 
         [TestMethod]
-        public void CalculateWinRate_ZeroWinsAndLosses()
+        public void Statistics_CalculateWinRate_ZeroWinsAndLosses()
         {
             int wins = 0;
             int losses = 0;
@@ -69,7 +69,7 @@ namespace WinRateTrackerTests
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void CalculateWinRate_NegativeWins()
+        public void Statistics_CalculateWinRate_NegativeWins()
         {
             int wins = -25;
             int losses = 50;
@@ -78,7 +78,7 @@ namespace WinRateTrackerTests
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void CalculateWinRate_NegativeLosses()
+        public void Statistics_CalculateWinRate_NegativeLosses()
         {
             int wins = 50;
             int losses = -25;
@@ -87,7 +87,7 @@ namespace WinRateTrackerTests
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void CalculateWinRate_NegativeWinsAndLosses()
+        public void Statistics_CalculateWinRate_NegativeWinsAndLosses()
         {
             int wins = -25;
             int losses = -25;
